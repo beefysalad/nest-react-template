cc2c95cb99cecc46959ecc32349d1e4f5fb1a4f8
John Patrick <jpatrickzephyr@gmail.com>
2022-09-11 16:17:58 +0800

    chore: add openapi/swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Beefy Template')
+    .setDescription('Beefy Fullstack Template')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(5000);
 }
 bootstrap();
